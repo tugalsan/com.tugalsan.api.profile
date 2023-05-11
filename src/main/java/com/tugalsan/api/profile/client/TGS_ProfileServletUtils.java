8e81a9dd6159385f3460ad1e543fd5cc124a2507
tugalsan karabacak <tugalsan@gmail.com>
2023-05-11 09:53:27 +0300

    .

--- src/main/java/com/tugalsan/api/profile/client/TGS_ProfileServletUtils.java
+++ src/main/java/com/tugalsan/api/profile/client/TGS_ProfileServletUtils.java
@@ -1,12 +1,13 @@
 package com.tugalsan.api.profile.client;
 
+import com.tugalsan.api.url.client.TGS_Url;
 import com.tugalsan.api.url.client.parser.*;
 
 public class TGS_ProfileServletUtils {
 
     final public static String SERVLET_NAME = "monitoring";//HARD-CODED IN LIB, THIS CANNOT BE CHANGED!
 
-    public static TGS_UrlParser URL_SERVLET(CharSequence url) {
+    public static TGS_UrlParser URL_SERVLET(TGS_Url url) {
         var urlMonitoring = TGS_UrlParser.of(url);
         urlMonitoring.path.fileOrServletName = TGS_ProfileServletUtils.SERVLET_NAME;
         var appName = urlMonitoring.path.paths.get(0);
